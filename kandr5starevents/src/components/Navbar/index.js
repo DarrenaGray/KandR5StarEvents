ea27af2a3cb2fdac234ee7b707ecf01414bf59d7
darrenagray <darrenag35@gmail.com>
2022-07-16 15:38:52 -0400

    adding info for navigation and styling

---
+++ kandr5starevents/src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam
+      quisquam corporis sed similique, possimus nemo rem a assumenda aspernatur
+      voluptates excepturi dolores deserunt nesciunt, accusantium quidem
+      molestias, laboriosam illo ab!
+    </>
+  );
+};
+
+export default Navbar;
