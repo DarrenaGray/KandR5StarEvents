2a8fd9777f7d80278015a85c8bbad3658fb0fab5
darrenagray <darrenag35@gmail.com>
2022-07-16 15:37:21 -0400

    added a note for updated router dom format

--- kandr5starevents/src/App.js
+++ kandr5starevents/src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
+        {/* React-Router-Dom v6 "Switch" is replaced by "Routes", "component" replaced by "element", and page object {Home} is now {<Home />}  */}
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
