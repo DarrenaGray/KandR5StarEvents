fa54d159871410daee4c719c010ce703d3bd7e6f
darrenagray <darrenag35@gmail.com>
2022-07-18 20:48:25 -0400

    initial nav setup/styling and rendered

--- kandr5starevents/src/components/Navbar/index.js
+++ kandr5starevents/src/components/Navbar/index.js
@@ -1,12 +1,41 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
+
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements";
 
 const Navbar = () => {
   return (
     <>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam
-      quisquam corporis sed similique, possimus nemo rem a assumenda aspernatur
-      voluptates excepturi dolores deserunt nesciunt, accusantium quidem
-      molestias, laboriosam illo ab!
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">kandr</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="home">Home</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="contact">Contact</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
     </>
   );
 };
