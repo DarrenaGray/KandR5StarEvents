5f43f95f60df805c1100e55cdc4f8a9f20dc1d75
darrenagray <darrenag35@gmail.com>
2022-08-01 18:06:17 -0400

    imported data objects for rendering

--- kandr5starevents/src/pages/index.js
+++ kandr5starevents/src/pages/index.js
@@ -3,14 +3,17 @@ import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 import Services from "../components/Services";
 import InfoSection from "../InfoSection";
+import { homeObjOne, homeObjTwo, homeObjThree } from "../InfoSection/Data";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
       <Services />
-      <InfoSection />
+      <InfoSection {...homeObjThree} />
     </>
   );
 };
