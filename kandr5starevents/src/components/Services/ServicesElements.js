217988a892cb9ba9352f603329c8abcdc0a9f056
darrenagray <darrenag35@gmail.com>
2022-08-01 14:39:29 -0400

    Added additional info and styling

--- kandr5starevents/src/components/Services/ServicesElements.js
+++ kandr5starevents/src/components/Services/ServicesElements.js
@@ -10,26 +10,44 @@ export const ServicesContainer = styled.div`
 
 export const ServicesWrapper = styled.div`
   max-width: 1000px;
+  margin: 0 auto;
+  display: grid;
+  /* fr is fractional unit and 1fr is for 1 part of the available space */
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  grid-gap: 16px;
+  padding: 0 50px;
 `;
 
 export const ServicesCard = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: 10px;
+  max-height: 340px;
+  padding: 30px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+  transition: all 0.2s ease-in-out;
 `;
 
 export const ServicesIcon = styled.img`
   height: 160px;
+  width: 160px;
+  margin-bottom: 10px;
 `;
 
 export const ServicesH1 = styled.h1`
   font-size: 2.5rem;
-  color: #fff;
+  margin-bottom: 64px;
 `;
 
 export const ServicesH2 = styled.h2`
   font-size: 1rem;
+  margin-bottom: 10px;
 `;
 
 export const ServicesP = styled.p`
   font-size: 1rem;
+  text-align: center;
 `;
