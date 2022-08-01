eb738c91ad99851436435c3f5f7410a067fd1893
darrenagray <darrenag35@gmail.com>
2022-08-01 14:11:32 -0400

    Routed services to pages for rendering

--- kandr5starevents/src/pages/index.js
+++ kandr5starevents/src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
+import Services from "../components/Services";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Services />
     </>
   );
 };
