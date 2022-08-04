2fd5c03926f9b8116c068cc68f19ed91acde54e3
darrenagray <darrenag35@gmail.com>
2022-08-03 22:36:57 -0400

    imported sidebar and footer for rendering. added open/close functionality for mobile view nav icon

--- kandr5starevents/src/pages/index.js
+++ kandr5starevents/src/pages/index.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 import Services from "../components/Services";
 import InfoSection from "../InfoSection";
+import Sidebar from "../components/Sidebar";
 import { homeObjOne, homeObjTwo, homeObjThree } from "../InfoSection/Data";
+import Footer from "../components/Footer";
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <Navbar />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
       <HeroSection />
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
       <Services />
       <InfoSection {...homeObjThree} />
+      <Footer />
     </>
   );
 };
