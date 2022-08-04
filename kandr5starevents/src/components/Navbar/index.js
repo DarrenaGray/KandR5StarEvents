57a06f983bbd9d4fcb2e441a39c88526bd4add58
darrenagray <darrenag35@gmail.com>
2022-08-03 21:47:00 -0400

    added functionality to links. changed navlogo from text to image

--- kandr5starevents/src/components/Navbar/index.js
+++ kandr5starevents/src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
+import { animateScroll as scroll } from "react-scroll";
 
 import {
   Nav,
@@ -11,27 +12,90 @@ import {
   NavLinks,
 } from "./NavbarElements";
 
-const Navbar = () => {
+import Logo from "../../images/krblsqfab.jpg";
+
+const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    // On scroll if the navbar is >= 80px then the navbar is black else its transparent
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  // To prevent erratic behvior when scrolling
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
+  console.log(Logo);
+
   return (
     <>
-      <Nav>
+      <Nav scrollNav={scrollNav}>
         <NavbarContainer>
-          <NavLogo to="/">K and R 5 Star Events</NavLogo>
-          <MobileIcon>
+          <NavLogo to="/" onClick={toggleHome} src={Logo} alt="logo"></NavLogo>
+
+          <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
           <NavMenu>
             <NavItem>
-              <NavLinks to="home">Home</NavLinks>
+              {/* spy tells you what you're clicking on */}
+              {/* offset is set to the same value as margin-top of Nav in NavbarElements in order to match up scroll exactly when link scroll changes */}
+              <NavLinks
+                to="home"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Home
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="about">About</NavLinks>
+              <NavLinks
+                to="about"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                About
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="services">Services</NavLinks>
+              <NavLinks
+                to="services"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Services
+              </NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="contact">Contact</NavLinks>
+              <NavLinks
+                to="contact"
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
+                Contact
+              </NavLinks>
             </NavItem>
           </NavMenu>
         </NavbarContainer>
