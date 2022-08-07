b039661817fd6438af79c18057d59bfbb73bf433
darrenagray <darrenag35@gmail.com>
2022-08-07 18:53:46 -0400

    moved infosection folder inside components folder

--- kandr5starevents/src/pages/index.js
+++ kandr5starevents/src/pages/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 import Services from "../components/Services";
-import InfoSection from "../InfoSection";
+import InfoSection from "../components/InfoSection";
 import Sidebar from "../components/Sidebar";
 import { homeObjOne, homeObjTwo, homeObjThree } from "../InfoSection/Data";
 import Footer from "../components/Footer";
