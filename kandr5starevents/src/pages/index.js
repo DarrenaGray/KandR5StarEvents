4b3ee4fe0ab836d24c00955bd70a64f4bc6f5636
darrenagray <darrenag35@gmail.com>
2022-08-11 17:42:12 -0400

    added functions from navbar to pages for passing down

--- kandr5starevents/src/pages/index.js
+++ kandr5starevents/src/pages/index.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { animateScroll as scroll } from "react-scroll";
+
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 import Services from "../components/Services";
@@ -12,11 +14,34 @@ import {
 import Footer from "../components/Footer";
 
 const Home = () => {
+  // States whether mobile icon is open or closed
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => {
     setIsOpen(!isOpen);
   };
+
+  // ---
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    // On scroll if the navbar is >= 80px then the navbar is black else its transparent
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  // To prevent erratic behvior when scrolling
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
   return (
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
@@ -24,7 +49,7 @@ const Home = () => {
       <HeroSection />
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
-      <Services />
+      <Services scrollNav={scrollNav} toggle={toggle} toggleHome={toggleHome} />
       <InfoSection {...homeObjThree} />
       <Footer />
     </>
